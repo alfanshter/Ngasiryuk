4e87d2cb889ff375b3929dee1f94c525112e8092
unknown <anampict@gmail.com>
2026-02-03 12:29:26 +0700

    init project

---
+++ app/src/main/java/com/example/ngasiryuk/commond/Color.kt
@@ -0,0 +1,5 @@
+package com.example.ngasiryuk.commond
+
+import androidx.compose.ui.graphics.Color
+
+val orange = Color(0xffFBC020)
---
+++ app/src/main/java/com/example/ngasiryuk/commond/Font.kt
@@ -0,0 +1,14 @@
+package com.example.galonqu.commond
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.ngasiryuk.R
+
+val plusjakarta = FontFamily(
+    Font(R.font.plusjakartasans_bold, FontWeight.Bold),
+    Font(R.font.plusjakartasans_medium, FontWeight.Medium),
+    Font(R.font.plusjakartasans_semibold, FontWeight.SemiBold),
+    Font(R.font.plusjakartasans_regular, FontWeight.Normal),
+
+    )
